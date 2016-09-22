73d8b271ed0ce69cd8f39e796d85d330f4feebe0
neilpeterson <neilpeterson@outlook.com>
2016-09-22 15:21:35 -0700

    inital windows files

---
+++ dotnet-core-music-windows/support-scripts/configure-music-app.ps1
@@ -0,0 +1,44 @@
+<#
+.SYNOPSIS
+.DESCRIPTION
+.PARAMETER Alias
+.EXAMPLE
+#>
+
+Param (
+    [string]$user,
+    [string]$password,
+    [string]$sqlserver
+)
+
+# firewall
+netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
+
+# folders
+New-Item -ItemType Directory c:\temp
+New-Item -ItemType Directory c:\music
+
+# install iis
+Install-WindowsFeature web-server -IncludeManagementTools
+
+# install dot.net core sdk
+Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=809122 -outfile c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe
+Start-Process c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe -ArgumentList '/quiet' -Wait
+Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\temp\DotNetCore.WindowsHosting.exe
+Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
+
+# download / config music app
+Invoke-WebRequest  https://github.com/neilpeterson/nepeters-azure-templates/raw/master/dotnet-core-music-vm-sql-db/music-app/music-store-azure-demo-pub.zip -OutFile c:\temp\musicstore.zip
+Expand-Archive C:\temp\musicstore.zip c:\music
+(Get-Content C:\music\config.json) | ForEach-Object { $_ -replace "<replaceserver>", $sqlserver } | Set-Content C:\music\config.json
+(Get-Content C:\music\config.json) | ForEach-Object { $_ -replace "<replaceuser>", $user } | Set-Content C:\music\config.json
+(Get-Content C:\music\config.json) | ForEach-Object { $_ -replace "<replacepass>", $password } | Set-Content C:\music\config.json
+
+# workaround for db creation bug
+Start-Process 'C:\Program Files\dotnet\dotnet.exe' -ArgumentList 'c:\music\MusicStore.dll'
+
+#configure iis
+Remove-WebSite -Name "Default Web Site"
+Set-ItemProperty IIS:\AppPools\DefaultAppPool\ managedRuntimeVersion ""
+New-Website -Name "MusicStore" -Port 80 -PhysicalPath C:\music\ -ApplicationPool DefaultAppPool
+& iisreset
