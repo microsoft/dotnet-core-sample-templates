8e4b6d4631b382d6d36e3c1c61e9abd193af9d52
Neil Peterson <neilpeterson@outlook.com>
2016-11-06 20:42:22 -0800

    Update configure-music-app.ps1

--- dotnet-core-music-windows/scripts/configure-music-app.ps1
+++ dotnet-core-music-windows/scripts/configure-music-app.ps1
@@ -1,25 +1,20 @@
-<#
-    .SYNOPSIS
-        Downloads and configures .Net Core Music Store application sample across IIS and Azure SQL DB.    
-#>
-
 Param (
     [string]$user,
     [string]$password,
     [string]$sqlserver
 )
 
-# Firewall
+# firewall
 netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
 
-# Folders
+# folders
 New-Item -ItemType Directory c:\temp
 New-Item -ItemType Directory c:\music
 
-# Install iis
+# install iis
 Install-WindowsFeature web-server -IncludeManagementTools
 
-# Install dot.net core sdk
+# install dot.net core sdk
 Invoke-WebRequest http://go.microsoft.com/fwlink/?LinkID=615460 -outfile c:\temp\vc_redistx64.exe
 Start-Process c:\temp\vc_redistx64.exe -ArgumentList '/quiet' -Wait
 Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=809122 -outfile c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe
@@ -27,14 +22,17 @@ Start-Process c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe -ArgumentList '/quie
 Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\temp\DotNetCore.WindowsHosting.exe
 Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
 
-# Download music app
+# download / config music app
 Invoke-WebRequest  https://github.com/Microsoft/dotnet-core-sample-templates/raw/master/dotnet-core-music-windows/music-app/music-store-azure-demo-pub.zip -OutFile c:\temp\musicstore.zip
 Expand-Archive C:\temp\musicstore.zip c:\music
+(Get-Content C:\music\config.json) | ForEach-Object { $_ -replace "<replaceserver>", $sqlserver } | Set-Content C:\music\config.json
+(Get-Content C:\music\config.json) | ForEach-Object { $_ -replace "<replaceuser>", $user } | Set-Content C:\music\config.json
+(Get-Content C:\music\config.json) | ForEach-Object { $_ -replace "<replacepass>", $password } | Set-Content C:\music\config.json
 
-# Azure SQL connection sting in environment variable
-[Environment]::SetEnvironmentVariable("Data:DefaultConnection:ConnectionString", "Server=$sqlserver;Database=MusicStore;Integrated Security=False;User Id=$user;Password=$password;MultipleActiveResultSets=True;Connect Timeout=30",[EnvironmentVariableTarget]::Machine)
+# workaround for db creation bug
+Start-Process 'C:\Program Files\dotnet\dotnet.exe' -ArgumentList 'c:\music\MusicStore.dll'
 
-# Configure iis
+#configure iis
 Remove-WebSite -Name "Default Web Site"
 Set-ItemProperty IIS:\AppPools\DefaultAppPool\ managedRuntimeVersion ""
 New-Website -Name "MusicStore" -Port 80 -PhysicalPath C:\music\ -ApplicationPool DefaultAppPool
