e37817c805a73b305faf13fa97d21bd92873850b
Pavel Morshenyuk <ashkue@outlook.com>
2018-05-02 18:24:22 +0200

    Removed some warnings

--- dotnet-core-music-linux/scripts/config-music.sh
+++ dotnet-core-music-linux/scripts/config-music.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# remove console warnings
+export DEBIAN_FRONTEND=noninteractive
+
 # install dotnet core
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo add-apt-repository universe
 sudo apt-get update
 sudo apt-get install -y dotnet-dev-1.1.4
 
@@ -17,19 +21,15 @@ sudo apt-get install -y nginx
 sudo service nginx start
 sudo touch /etc/nginx/sites-available/default
 sudo wget https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/nginx-config/default -O /etc/nginx/sites-available/default
-#sudo cp /opt/music/nginx-config/default /etc/nginx/sites-available/
 sudo nginx -s reload
 
 # update and secure music config file
 sed -i "s/<replaceserver>/$1/g" /opt/music/config.json
 sed -i "s/<replaceuser>/$2/g" /opt/music/config.json
 sed -i "s/<replacepass>/$3/g" /opt/music/config.json
-#sudo chown $2 /opt/music/config.json
-#sudo chmod 0400 /opt/music/config.json
+sudo chmod 0777 /opt/music/config.json
 
 # config supervisor
-sudo add-apt-repository universe
-sudo apt-get update
 sudo apt-get install -y supervisor
 sudo touch /etc/supervisor/conf.d/music.conf
 sudo wget https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/supervisor/music.conf -O /etc/supervisor/conf.d/music.conf
