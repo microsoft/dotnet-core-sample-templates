ac81adc3620d0296337f0d626cd6b402d9606c99
Pavel Morshenyuk <ashkue@outlook.com>
2018-05-02 17:32:31 +0200

    Set automatic proceed

--- dotnet-core-music-linux/scripts/config-music.sh
+++ dotnet-core-music-linux/scripts/config-music.sh
@@ -5,7 +5,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
-sudo apt-get install dotnet-dev-1.1.4
+sudo apt-get install -y dotnet-dev-1.1.4
 
 # download application
 sudo wget https://raw.github.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/music-store-azure-demo-pub.tar /
